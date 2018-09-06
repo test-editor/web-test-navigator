d247c6c8edbb4408f727832641af90271d0a73cc
Jan Jelschen <jan.jelschen@itemis.de>
2018-09-06 08:15:52 +0200

    remove unused utility functions

--- src/app/modules/model/filters.ts
+++ src/app/modules/model/filters.ts
@@ -13,21 +13,6 @@ import { ElementType } from '../persistence-service/workspace-element';
   export const testNavigatorFilter = (node: TreeNode) =>
     (node as TestNavigatorTreeNode).type === ElementType.Folder || isTestEditorFile(node.id);
 
-  export const tslFilter = (node: TreeNode) =>
-    (node as TestNavigatorTreeNode).type === ElementType.Folder || isTslFile(node.id);
-
-  export const tclFilter = (node: TreeNode) =>
-    (node as TestNavigatorTreeNode).type === ElementType.Folder || isTclFile(node.id);
-
-  export const configFilter = (node: TreeNode) =>
-    (node as TestNavigatorTreeNode).type === ElementType.Folder || isConfigFile(node.id);
-
-  export const tmlFilter = (node: TreeNode) =>
-    (node as TestNavigatorTreeNode).type === ElementType.Folder || isTmlFile(node.id);
-
-  export const amlFilter = (node: TreeNode) =>
-    (node as TestNavigatorTreeNode).type === ElementType.Folder || isAmlFile(node.id);
-
   export function isTestEditorFile(path: string) {
     return isTslFile(path) || isTclFile(path) || isConfigFile(path) || isTmlFile(path) || isAmlFile(path); }
 
