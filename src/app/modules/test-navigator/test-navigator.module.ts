4e756df4ccf9e2478bc4fc0e4dc5e83a1ad6cfad
Jan Jelschen <jan.jelschen@itemis.de>
2018-09-10 09:30:01 +0200

    ng g component modules/FilterBar --module modules/test-navigator/test-navigator.module.ts

--- src/app/modules/test-navigator/test-navigator.module.ts
+++ src/app/modules/test-navigator/test-navigator.module.ts
@@ -6,13 +6,15 @@ import { TreeFilterService } from '../tree-filter-service/tree-filter.service';
 import { HttpProviderService } from '../http-provider-service/http-provider.service';
 import { TreeViewerModule } from '@testeditor/testeditor-commons';
 import { PersistenceServiceConfig } from '../persistence-service/persistence.service.config';
+import { FilterBarComponent } from '../filter-bar/filter-bar/filter-bar.component';
 
 @NgModule({
   imports: [
     CommonModule, TreeViewerModule
   ],
   declarations: [
-    TestNavigatorComponent
+    TestNavigatorComponent,
+    FilterBarComponent
   ],
   exports: [
     TestNavigatorComponent
