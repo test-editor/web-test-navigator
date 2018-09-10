0d05e80dfa1e76591368a9a260f96eee3c2ff4cf
Jan Jelschen <jan.jelschen@itemis.de>
2018-09-10 11:27:54 +0200

    emit event when filter state changes

--- src/app/modules/filter-bar/filter-bar.component.spec.ts
+++ src/app/modules/filter-bar/filter-bar.component.spec.ts
@@ -1,27 +1,61 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { Component, ViewChild } from '@angular/core';
+import { async, ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core/testing';
 import { FormsModule } from '@angular/forms';
+import { By } from '@angular/platform-browser';
 import { ButtonsModule } from 'ngx-bootstrap/buttons';
-import { FilterBarComponent } from './filter-bar.component';
+import { FilterBarComponent, FilterState } from './filter-bar.component';
+
+@Component({
+  selector: `app-host-component`,
+  template: `<app-filter-bar (filtersChanged)="onFiltersChanged($event)"></app-filter-bar>`
+})
+class TestHostComponent {
+  public filterState: FilterState;
+
+  @ViewChild(FilterBarComponent)
+  public filterBarUnderTest: FilterBarComponent;
+
+  onFiltersChanged(state: FilterState) {
+    this.filterState = state;
+  }
+}
+
+
 
 fdescribe('FilterBarComponent', () => {
-  let component: FilterBarComponent;
-  let fixture: ComponentFixture<FilterBarComponent>;
+  let hostComponent: TestHostComponent;
+  let fixture: ComponentFixture<TestHostComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [ FormsModule, ButtonsModule.forRoot() ],
-      declarations: [ FilterBarComponent ]
+      declarations: [ TestHostComponent, FilterBarComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(FilterBarComponent);
-    component = fixture.componentInstance;
+    fixture = TestBed.createComponent(TestHostComponent);
+    hostComponent = fixture.componentInstance;
     fixture.detectChanges();
   });
 
   it('should create', () => {
-    expect(component).toBeTruthy();
+    expect(hostComponent.filterBarUnderTest).toBeTruthy();
   });
+
+  it('should fire an event passing on the filter state when a button is pressed', fakeAsync(() => {
+    // given
+    hostComponent.filterState = undefined;
+    const tslButton = fixture.debugElement.query(By.css('#filter-bar > label')).nativeElement;
+
+    // when
+    tslButton.click();
+    tick();
+
+    // then
+    expect(hostComponent.filterState.tsl).toBeTruthy();
+    expect(hostComponent.filterState.tcl).toBeFalsy();
+    expect(hostComponent.filterState.aml).toBeFalsy();
+  }));
 });
--- src/app/modules/filter-bar/filter-bar.component.ts
+++ src/app/modules/filter-bar/filter-bar.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+
+export interface FilterState { tsl: boolean; tcl: boolean; aml: boolean; }
 
 @Component({
   selector: 'app-filter-bar',
@@ -6,11 +8,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./filter-bar.component.css']
 })
 export class FilterBarComponent implements OnInit {
-  filters = { tsl: false, tcl: false, aml: false };
+  filters: FilterState = { tsl: false, tcl: false, aml: false };
+
+  @Output() filtersChanged: EventEmitter<FilterState> = new EventEmitter<FilterState>();
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onClick() {
+    this.filtersChanged.emit({tsl: this.filters.tsl, tcl: this.filters.tcl, aml: this.filters.aml});
   }
 
 }
