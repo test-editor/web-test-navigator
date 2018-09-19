2f011a02d9c713d5395822c273528210945dc7c2
Jan Jelschen <jan.jelschen@itemis.de>
2018-09-19 10:32:06 +0200

    disable rename button for root

--- src/app/modules/tree-filter-service/tree-filter.service.ts
+++ src/app/modules/tree-filter-service/tree-filter.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { TestNavigatorTreeNode } from '../model/test-navigator-tree-node';
 import { PersistenceService } from '../persistence-service/persistence.service';
+import { WorkspaceElement } from '../persistence-service/workspace-element';
 
 @Injectable()
 export class TreeFilterService {
@@ -11,10 +12,23 @@ export class TreeFilterService {
   async listTreeNodes(): Promise<TestNavigatorTreeNode> {
     return this.persistenceService.listFiles().then(
       (root) => {
-        const testNavigatorRoot = new TestNavigatorTreeNode(root);
-        return testNavigatorRoot.findFirst((node) => (node as TestNavigatorTreeNode).id === TreeFilterService.ROOT_PATH);
+        return new TestNavigatorTreeNode(this.findFirst(root, (element) => element.path === TreeFilterService.ROOT_PATH));
       });
   }
 
+  private findFirst(root: WorkspaceElement, condition: (element: WorkspaceElement) => boolean): WorkspaceElement {
+    let result: WorkspaceElement = null;
+    if (condition(root)) {
+      result = root;
+    } else {
+      for (const child of root.children) {
+        result = this.findFirst(child, condition);
+        if (result !== null) {
+          break;
+        }
+      }
+    }
+    return result;
+  }
 
 }
