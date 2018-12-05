a43b5f94670d865740ad98f427795602f7418bd7
Jan Jelschen <jan.jelschen@itemis.de>
2018-12-05 11:24:59 +0100

    expose label and style providers on public API

--- public_api.ts
+++ public_api.ts
@@ -3,4 +3,6 @@ export { PersistenceServiceConfig } from './src/app/modules/persistence-service/
 export { IndexServiceConfig } from './src/app/modules/index-service/index.service.config';
 export { ValidationMarkerServiceConfig } from './src/app/modules/validation-marker-service/validation-marker.service.config';
 export { WorkspaceElement, ElementType } from './src/app/modules/persistence-service/workspace-element';
+export { UserActivityLabelProvider } from './src/app/modules/style-provider/user-activity-label-provider';
+export { StyleProvider } from './src/app/modules/style-provider/style-provider';
 export * from './src/app/modules/event-types-out';
